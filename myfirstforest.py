ef16d133c79f617210d9cc2d40dc10c7b9121651
rmcdonnell <rmcdonnell1990@icloud.com>
2015-06-05 20:17:52 -0400

    updates and formatting changes because I'm neurotic.

--- myfirstforest.py
+++ myfirstforest.py
@@ -12,13 +12,14 @@ from sklearn.ensemble import RandomForestClassifier
 
 # Data cleanup
 # TRAIN DATA
-train_df = pd.read_csv('train.csv', header=0)        # Load the train file into
-                                                     # a dataframe
+train_df = pd.read_csv('train.csv', header=0)       # Load the train file into
+                                                    # a dataframe
 # I need to convert all strings to integer classifiers.
 # I need to fill in the missing values of the data and make it complete.
 
 # female = 0, Male = 1
-train_df['Gender'] = train_df['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
+mf_set = {'female': 0, 'male': 1}
+train_df['Gender'] = train_df['Sex'].map( mf_set ).astype(int)
 
 # Embarked from 'C', 'Q', 'S'
 # Note this is not ideal: in translating categories to numbers, Port "2" is
@@ -26,13 +27,16 @@ train_df['Gender'] = train_df['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
 
 # All missing Embarked -> just make them embark from most common place
 if len(train_df.Embarked[ train_df.Embarked.isnull() ]) > 0:
-    train_df.Embarked[ train_df.Embarked.isnull() ] = train_df.Embarked.dropna().mode().values
+    train_Embarked = train_df.Embarked.dropna().mode().values
+    train_df.loc[ train_df.Embarked.isnull(), 'Embarked' ] = train_Embarked
+#    train_df.Embarked[ train_df.Embarked.isnull() ] = temp_Embarked
 
-Ports = list(enumerate(np.unique(train_df['Embarked']))) # determine all values
-                                                         # of Embarked,
-Ports_dict = { name : i for i, name in Ports }        # set up a dictionary in
-                                                      # the form  Ports : index
-train_df.Embarked = train_df.Embarked.map( lambda x: Ports_dict[x]).astype(int)
+Ports = list(enumerate(np.unique(train_df['Embarked'])))# determine all values
+                                                        # of Embarked,
+Ports_dict = { name : i for i, name in Ports }          # set up a dictionary
+                                                        # in the form  Ports :
+                                                        # index
+train_df.Embarked = train_df.Embarked.map(lambda x: Ports_dict[x]).astype(int)
                                             # Convert all Embark strings to int
 
 # All the ages with no data -> make the median of all Ages
@@ -47,8 +51,8 @@ train_df = train_df.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId'],
                          axis=1) 
 
 # TEST DATA
-test_df = pd.read_csv('test.csv', header=0)           # Load the test file into
-                                                      # a dataframe
+test_df = pd.read_csv('test.csv', header=0)          # Load the test file into
+                                                     # a dataframe
 
 # I need to do the same with the test data now, so that the columns are the
 # same as the training data
@@ -59,7 +63,8 @@ test_df['Gender'] = test_df['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
 # Embarked from 'C', 'Q', 'S'
 # All missing Embarked -> just make them embark from most common place
 if len(test_df.Embarked[ test_df.Embarked.isnull() ]) > 0:
-    test_df.Embarked[ test_df.Embarked.isnull() ] = test_df.Embarked.dropna().mode().values
+    test_Embarked = test_df.Embarked.dropna().mode().values
+    test_df.Embarked[ test_df.Embarked.isnull() ] = test_Embarked
 # Again convert all Embarked strings to int
 test_df.Embarked = test_df.Embarked.map( lambda x: Ports_dict[x]).astype(int)
 
@@ -74,7 +79,8 @@ train_df['Age'] = train_df['Age']//5*5
 if len(test_df.Fare[ test_df.Fare.isnull() ]) > 0:
     median_fare = np.zeros(3)
     for f in range(0,3):                                         # loop 0 to 2
-        median_fare[f] = test_df[ test_df.Pclass == f+1 ]['Fare'].dropna().median()
+        temp = test_df[ test_df.Pclass == f+1 ]['Fare'].dropna().median()
+        median_fare[f] = temp
     for f in range(0,3):                                         # loop 0 to 2
         test_df.loc[ (test_df.Fare.isnull()) & 
                              (test_df.Pclass == f+1 ), 'Fare'] = median_fare[f]
